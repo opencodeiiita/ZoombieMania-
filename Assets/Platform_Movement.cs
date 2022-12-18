f3f7f63799bf757cbb6820213a00d3a063306f60
Atharv23m <atharv23m@gmail.com>
2022-12-18 17:08:56 +0530

    Updated for Issue#6

---
+++ Assets/Platform_Movement.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform_Movement : MonoBehaviour
+{
+    // The points between which the platform will oscillate
+    public Transform p1;
+    public Transform p2;
+
+    [SerializeField]
+    private float speed; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
