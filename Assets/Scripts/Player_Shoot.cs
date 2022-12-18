e939888a0f5e0fc662d4e5a3b656c587a9932ee0
Aastha <97506205+aasthaaaa7@users.noreply.github.com>
2022-12-18 14:32:40 +0530

    zombieeee

--- Assets/Scripts/Player_Shoot.cs
+++ Assets/Scripts/Player_Shoot.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Player_Shoot : MonoBehaviour
 {
+    public float rotationSpeed;
+    float playerAngle;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,8 @@ public class Player_Shoot : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        playerAngle += Input.GetAxis("Mouse X") * rotationSpeed * -Time.deltaTime;
+        playerAngle = Mathf.Clamp(playerAngle, 180, 360);
+        transform.localRotation = Quaternion.AngleAxis(playerAngle, Vector3.up);
     }
 }
