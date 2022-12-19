955d62a5cea12267b162f35edd8e6e537e5c76cf
Aastha <97506205+aasthaaaa7@users.noreply.github.com>
2022-12-19 12:12:55 +0530

    zombiee

--- Assets/Scripts/Player_Shoot.cs
+++ Assets/Scripts/Player_Shoot.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class Player_Shoot : MonoBehaviour
 {
+    private Transform playerTransform;
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerTransform = this.transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        var dir = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
+        var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(-angle, Vector3.up);
     }
 }
