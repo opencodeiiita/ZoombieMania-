b2e7959764f4d55b14f6cccca061fe4cbba6598f
TheDivineAurora <wolfenbitzyt@gmail.com>
2022-12-22 01:47:43 +0530

    changes to script

--- Assets/Platform_Movement.cs
+++ Assets/Platform_Movement.cs
@@ -1,25 +1,23 @@
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Platform_Movement : MonoBehaviour
-{
-    // The points between which the platform will oscillate
-    public Transform p1;
-    public Transform p2;
+{ 
+    [SerializeField] GameObject[] goThere;
+    int nextgoThere = 0;
 
-    [SerializeField]
-    private float speed; 
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    [SerializeField] float speed = 0.01f; 
+   
     void Update()
     {
-        
+         if( Vector3.Distance(transform.position , goThere[nextgoThere].transform.position) < 0.1f){
+            nextgoThere++;
+            if(nextgoThere >= goThere.Length){
+                nextgoThere = 0;
+            }
+         }
+         transform.position = Vector3.MoveTowards(transform.position, goThere[nextgoThere].transform.position,speed);
     }
 }
